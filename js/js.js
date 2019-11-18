df5f255b9738e679a62607570041e3a2b550bac1
AlexSem <aleksey.semenovich@gmail.com>
2019-11-18 20:44:13 +0200

    Калькулятор расходов

--- js/js.js
+++ js/js.js
@@ -68,19 +68,69 @@ optionalExpensesBtn.addEventListener('click', function() {
 });
 
 countBtn.addEventListener('click', function() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    dayBudgetValue.textContent = appData.moneyPerDay;
-
-    if (appData.moneyPerDay < 100) {
-        levelValue.textContent = "Это минимальный уровень достатка!";
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        levelValue.textContent ="Это средний уровень достатка!";
-    } else if (appData.moneyPerDay > 2000) {
-        levelValue.textContent = "Это высокий уровень достатка!";
+    if(appData.budget != undefined) {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        dayBudgetValue.textContent = appData.moneyPerDay;
+    
+        if (appData.moneyPerDay < 1000) {
+            levelValue.textContent = "Это минимальный уровень достатка!";
+        } else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 2001) {
+            levelValue.textContent ="Это средний уровень достатка!";
+        } else if (appData.moneyPerDay > 2000) {
+            levelValue.textContent = "Это высокий уровень достатка!";
+        } else {
+            levelValue.textContent = "Ошибочка...!";
+        }
     } else {
-        levelValue.textContent = "Ошибочка...!";
+        dayBudgetValue.textContent = "Не указан бюджет на месяц"
+    }
+
+});
+
+incomeItem.addEventListener('input', function() {
+    let items = incomeItem.value;
+    appData.income = items.split(", ");
+    incomeValue.textContent = appData.income;
+});
+
+checkSavings.addEventListener('click', function() {
+    if(appData.savings == true) {
+        appData.savings = false;
+        //console.log(appData.savings);
+    } else {
+        appData.savings = true;
+        //console.log(appData.savings);
     }
 });
+
+sumValue.addEventListener('input', function() {
+    if(appData.savings == true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+        
+        appData.monthIncome = sum/100/12*percent;
+        appData.yearIncome = sum/100*percent;
+
+        monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
+        yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
+    }
+    console.log(sumValue.value);
+    console.log(percentValue.value);
+});
+
+percentValue.addEventListener('input', function() {
+    if(appData.savings == true) {
+        let sum = +sumValue.value,
+            percent = +percentValue.value;
+    
+        appData.monthIncome = sum/100/12*percent;
+        appData.yearIncome = sum/100*percent;
+
+        monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
+        yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
+    }
+    console.log(appData.monthIncome);
+ });
     
 let appData = {
     budget: money,
@@ -88,33 +138,7 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true,
-    checkSavings: function () {
-        if (appData.savings == true) {
-            let save = +prompt("Какова сумма накоплений?"),
-                percent = +prompt("Под какой процент?");
-    
-                appData.monthIncome = save/100/12*percent;
-                alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
-        }
-    },
-    chooseIncome: function () {
-
-        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
-
-        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
-            console.log("Вы ввели некорректные данные или не ввели их вовсе");
-        } else {
-            appData.income = items.split(", ");
-            appData.income.push(prompt("Может что-то еще?"));
-            appData.income.sort();
-        }
-
-        appData.income.forEach (function (itemmassive, i) {
-            alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive);
-        });
-
-    }
+    savings: false
 };
 
 // for (let key in appData) {
