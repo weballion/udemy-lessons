06952b92ce536619dfe20c16c9270802f41e80b1
AlexSem <aleksey.semenovich@gmail.com>
2019-11-18 20:44:45 +0200

    Калькулятор расходов

--- js/js.js
+++ js/js.js
@@ -140,7 +140,3 @@ let appData = {
     income: [],
     savings: false
 };
-
-// for (let key in appData) {
-//     console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
-// }
